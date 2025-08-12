af67219556ddecb55c919d6d99fef004d6ed8115
DerekSturm263 <dereksturm263@gmail.com>
2025-08-12 12:34:48 -0700

    f

--- src/app/skills/[slug]/page.tsx
+++ src/app/skills/[slug]/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import fs from 'fs';
 import { useParams } from 'next/navigation';
+import fs from 'fs';
 
 /*const lti = require('ltijs').Provider;
 
@@ -27,7 +27,7 @@ export default function Page() {
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[250px] md:h-[400px]"
-            href="/lesson"
+            href="../learn"
             target="_self"
             rel="noopener noreferrer"
           >
@@ -35,7 +35,7 @@ export default function Page() {
           </a>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[250px] md:h-[400px]"
-            href="/practice"
+            href="./practice"
             target="_self"
             rel="noopener noreferrer"
           >
