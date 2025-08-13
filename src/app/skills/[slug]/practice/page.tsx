1d415f1e78056c4bcca3f149b32b9356e29b4e56
DerekSturm263 <dereksturm263@gmail.com>
2025-08-12 22:22:01 -0700

    n

--- src/app/skills/[slug]/practice/page.tsx
+++ src/app/skills/[slug]/practice/page.tsx
@@ -1,9 +1,16 @@
+'use client'
+
+import { useParams } from 'next/navigation';
+import sql from '../../../lib/sql';
+
 export default function Page() {
+  const params = useParams();
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1 className="text-center sm:text-center">
-          Practice
+          {params.slug} - Practice
         </h1>
       </main>
     </div>
