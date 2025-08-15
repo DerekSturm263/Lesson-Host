75db58d0c990ad67d0c11ae5bb4f323cf1479b1b
DerekSturm263 <dereksturm263@gmail.com>
2025-08-14 23:57:26 -0700

    help

--- src/app/lib/files.ts
+++ src/app/lib/files.ts
@@ -1,16 +1,19 @@
 'use server'
 
-import { readFile } from 'fs/promises';
+import { promises as fs } from 'fs';
 import { Skill, Course, Project } from './types';
 
 export async function getSkill(name: string) : Promise<Skill> {
-  return JSON.parse(await readFile(`${process.cwd()}/app/data/skills/${name}.json`, 'utf-8')) as Skill;
+  const file = await fs.readFile(`${process.cwd()}/app/data/skills/${name}.json`, 'utf-8')
+  return JSON.parse(file) as Skill;
 }
 
 export async function getProject(name: string) : Promise<Project> {
-  return JSON.parse(await readFile(`${process.cwd()}/app/data/projects/${name}.json`, 'utf-8')) as Project;
+  const file = await fs.readFile(`${process.cwd()}/app/data/projects/${name}.json`, 'utf-8')
+  return JSON.parse(file) as Project;
 }
 
 export async function getCourse(name: string) : Promise<Course> {
-  return JSON.parse(await readFile(`${process.cwd()}/app/data/courses/${name}.json`, 'utf-8')) as Course;
+  const file = await fs.readFile(`${process.cwd()}/app/data/courses/${name}.json`, 'utf-8');
+  return JSON.parse(file) as Course;
 }
