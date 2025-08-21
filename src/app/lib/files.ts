bca630c54f4d9a9a5f203afc78d47818ddcdff52
DerekSturm263 <dereksturm263@gmail.com>
2025-08-21 00:57:02 -0700

    aaaa

--- src/app/lib/files.ts
+++ src/app/lib/files.ts
@@ -3,17 +3,17 @@
 import { readFile } from 'fs/promises';
 import { Skill, Course, Project } from './types';
 
-export async function getSkill(name: string) : Promise<Skill> {
+export async function getSkill(name: string): Promise<Skill> {
   const file = await readFile(`${process.cwd()}/app/data/skills/${name}.json`, 'utf-8');
   return JSON.parse(file) as Skill;
 }
 
-export async function getProject(name: string) : Promise<Project> {
+export async function getProject(name: string): Promise<Project> {
   const file = await readFile(`${process.cwd()}/app/data/projects/${name}.json`, 'utf-8');
   return JSON.parse(file) as Project;
 }
 
-export async function getCourse(name: string) : Promise<Course> {
+export async function getCourse(name: string): Promise<Course> {
   const file = await readFile(`${process.cwd()}/app/data/courses/${name}.json`, 'utf-8');
   return JSON.parse(file) as Course;
 }
