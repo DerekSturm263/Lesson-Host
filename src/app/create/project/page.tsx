c897b05f49e09219af36a27692d2d6142351c30e
DerekSturm263 <dereksturm263@gmail.com>
2025-08-25 17:14:45 -0700

    a

--- src/app/create/project/page.tsx
+++ src/app/create/project/page.tsx
@@ -2,7 +2,7 @@ import { Header } from '../../lib/components';
 import { createProject } from '../../lib/database';
 
 export default async function Page({ params }: { params: Promise<{ slug: string }>}) {
-  const project = await createProject();
+  //const project = await createProject();
 
   return (
     <div>
--- src/app/create/skill/page.tsx
+++ src/app/create/skill/page.tsx
@@ -2,7 +2,7 @@ import { Header } from '../../lib/components';
 import { createSkill } from '../../lib/database';
 
 export default async function Page({ params }: { params: Promise<{ slug: string }>}) {
-  const skill = await createSkill();
+  //const skill = await createSkill();
   
   return (
     <div>
