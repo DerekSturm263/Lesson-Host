25dfbb52e843921e35a623762f5f6d36496e9e18
DerekSturm263 <dereksturm263@gmail.com>
2025-08-29 00:44:08 -0700

    l

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -345,7 +345,9 @@ function Codespace({ elementID }: { elementID: types.ElementID }) {
 
     console.log(response);
 
-    setOutput(response.stdout ?? response.stderr ?? 'Program did not output anything');
+    const output = response.stdout + '\n\n' + response.stderr;
+
+    setOutput(output == '\n\n' ? 'Program did not output anything' : output);
 
     const feedback = await verifyCodespace(helpers.getElement(elementID).text, content, response, helpers.getInteractionValue<types.Codespace>(elementID).correctOutput ?? '', helpers.getInteractionValue<types.Codespace>(elementID).language);
     helpers.setText(elementID, feedback.feedback);
