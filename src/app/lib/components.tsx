3cdb7e4f129c194d5c17a5b317478672b85859d8
DerekSturm263 <dereksturm263@gmail.com>
2025-08-29 02:36:05 -0700

    l

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -426,11 +426,11 @@ function Text({ elementID }: { elementID: types.ElementID }) {
         className="text"
       >
         <Markdown>
-          {JSON.stringify((<TypeAnimation
+          {(<TypeAnimation
             sequence={[ text ]}
             speed={70}
             cursor={false}
-          />))}
+          />).props.sequence[0]}
         </Markdown>
       </div>
 
