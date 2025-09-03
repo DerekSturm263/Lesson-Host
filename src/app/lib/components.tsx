8c09bfbe6d4a576d94fa9425013e91104ef44e36
Derek <dereksturm263@gmail.com>
2025-09-03 19:22:57 +0000

    c

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -426,6 +426,9 @@ function Text({ elementID }: { elementID: types.ElementID }) {
       >
         <Markdown
           components={{
+            p({ node, children }) {
+              return <WordWrapper text={String(children)} />
+            }
             strong({ node, children }) {
               return <strong><WordWrapper text={String(children)} /></strong>
             },
