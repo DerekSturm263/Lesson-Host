b65a09a22cac8977664f5c71b0734c7e773a54b1
Derek <dereksturm263@gmail.com>
2025-10-03 17:30:19 -0700

    a

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -929,7 +929,7 @@ function Codespace({ elementID, isDisabled, mode }: { elementID: types.ElementID
           <Typography
             variant="body1"
           >
-            Press \"Run\" to execute your code. Any outputs or errors will be printed here
+            Press "Run" to execute your code. Any outputs or errors will be printed here
           </Typography>
 
           <Button
