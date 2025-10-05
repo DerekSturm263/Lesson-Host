041d22b819e8a3502e432c90958192812ff99dad
Derek <dereksturm263@gmail.com>
2025-10-04 23:14:00 -0700

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,17 @@
 import { Metadata } from "next";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-
-import "./globals.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
-const darkTheme = createTheme({
-  palette: {
-    primary: {
-      light: '#43b1e4',
-      main: '#3490c2',
-      dark: '#265f8c',
-      contrastText: '#fff',
-    },
-    secondary: {
-      light: '#d8863c',
-      main: '#c26634',
-      dark: '#9c5f3d',
-      contrastText: '#fff',
-    },
-  },
-  typography: {
-    fontFamily: [
-      'Lexend',
-      'Roboto',
-      'Arial',
-      'Helvetica',
-      'sans-serif'
-    ].join(','),
-    fontSize: 12
-  }
-});
+import theme from "@/app/theme";
 
 export const metadata: Metadata = {
   title: "MySkillStudy.com",
   description: "Learn anything by practicing skills and creating projects.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         
         <body>
