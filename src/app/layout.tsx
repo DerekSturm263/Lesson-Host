21daf28c19b917ec2c3862e4e694717e97fadfaa
Derek <dereksturm263@gmail.com>
2025-10-04 22:55:59 -0700

    a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,6 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
 
 const lexend = Lexend({
   variable: "--font-lexend",
@@ -31,15 +23,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        
-        <body
-          className={`${lexend.variable} antialiased`}
-        >
-          {children}
-        </body>
-      </ThemeProvider>
+      <body
+        className={`${lexend.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
