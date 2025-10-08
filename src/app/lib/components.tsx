788ac7092c33044dfc37a4965e2203479c480ffa
Derek <dereksturm263@gmail.com>
2025-10-08 19:39:08 +0000

    a

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -284,6 +284,7 @@ export function LearnPageContent({ slug, learn, mode, apiKey }: { slug: string, 
   }
 
   useEffect(() => {
+    console.log(helpers.getAbsoluteIndex(thisElement));
     console.log(JSON.stringify(elementsUnlocked));
 
     window.addEventListener(`updateElement`, (e: Event) => {
@@ -296,6 +297,7 @@ export function LearnPageContent({ slug, learn, mode, apiKey }: { slug: string, 
         setIsSnackbarOpen(true);
       }
 
+      console.log(helpers.getAbsoluteIndex(thisElement));
       console.log(JSON.stringify(newElementsUnlocked));
     });
 
