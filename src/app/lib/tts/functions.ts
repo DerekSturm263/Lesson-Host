45a164e73350bc4d9f75d87169d4f8f4a067b50c
Derek <dereksturm263@gmail.com>
2025-10-09 18:25:24 +0000

    a

--- src/app/lib/tts/functions.ts
+++ src/app/lib/tts/functions.ts
@@ -1,21 +1,24 @@
 'use server'
 
-import textToSpeech from '@google-cloud/text-to-speech';
+import { TextToSpeechClient } from '@google-cloud/text-to-speech';
 
-const client = new textToSpeech.TextToSpeechClient();
+const client = new TextToSpeechClient();
 
-export default async function speakText(text: string) {
+export default function speakText(text: string) {
   const request = {
     input: { text: text },
     voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
     audioConfig: { audioEncoding: 'MP3' }
-  }
+  };
 
   const stream = client.streamingSynthesize();
+  client.synthesizeSpeech()
 
   stream.on('data', (response) => { console.log(response) });
   stream.on('error', (err) => { throw(err) });
   stream.on('end', () => { });
   stream.write(request);
   stream.end();
+
+  console.log(text);
 }
