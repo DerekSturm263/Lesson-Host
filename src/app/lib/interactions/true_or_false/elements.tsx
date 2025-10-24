963ee0c2332513b05739f31432bfcb8eaeba0515
Derek <dereksturm263@gmail.com>
2025-10-24 21:17:04 +0000

    d

--- src/app/lib/interactions/true_or_false/elements.tsx
+++ src/app/lib/interactions/true_or_false/elements.tsx
@@ -52,7 +52,7 @@ function Component(props: InteractionProps) {
 
   return (
     <Box
-      sx={{ flexGrow: 1, alignItems: "center", alignContent: "center" }}
+      sx={{ flexGrow: 1, alignSelf: "center", alignContent: "center" }}
     >
       <FormControl
         id={`interaction${helpers.getAbsoluteIndex(props.elementID)}`}
