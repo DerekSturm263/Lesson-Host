943cdfc6eb650fd7b04d7ad0139667cf6cae0dcd
Derek <dereksturm263@gmail.com>
2025-10-29 15:21:51 +0000

    d

--- src/app/lib/interactions/engine/elements.tsx
+++ src/app/lib/interactions/engine/elements.tsx
@@ -2,13 +2,47 @@
 
 import { InteractionPackage, InteractionProps } from '@/app/lib/types';
 import { Type } from '@google/genai';
+import { CSSProperties, JSX, PureComponent, useState } from 'react';
+import { GameEngine } from 'react-game-engine';
+import * as helpers from '@/app/lib/helpers';
 
 export type InteractionType = {
-  placeholder: boolean
+  properties: CSSProperties,
+  entities: Entity[]
 };
 
+export type Entity = {
+  id: string,
+  transform: transform,
+  properties: CSSProperties,
+  renderer: PureComponent | undefined
+};
+
+export type transform = {
+  position: vector2,
+  rotation: number,
+  scale: vector2
+}
+
+export type vector2 = {
+  x: number,
+  y: number
+}
+
 const defaultValue: InteractionType = {
-  placeholder: false
+  properties: { backgroundColor: "blue" },
+  entities: [
+    {
+      id: "e1",
+      transform: {
+        position: { x: 0, y: 0 },
+        rotation: 0,
+        scale: { x: 1, y: 1 }
+      },
+      properties: { backgroundColor: "red" },
+      renderer: undefined
+    }
+  ]
 }
 
 const schema = {
@@ -25,13 +59,42 @@ const schema = {
 };
 
 function Component(props: InteractionProps) {
+  const [ properties, setProperties ] = useState(helpers.getInteractionValue<InteractionType>(props.elementID).properties);
+  const [ entities, setEntities ] = useState(helpers.getInteractionValue<InteractionType>(props.elementID).entities);
+
+  //const renderedEntities = entities.map(entity => ({ ...entity, renderer: Renderer }));
+
   return (
-    <iframe
-      className="fullscreenInteraction"
-      src="https://editor.godotengine.org/releases/latest/"
-    ></iframe>
+    <GameEngine
+      style={{ ...properties, width: "100%", height: "100%" }}
+      systems={[]}
+      entities={entities}
+    />
   );
 }
+
+/*function MoveEntity(entities, { input }) {
+  const { payload } = input.find(x => x.name === "onMouseDown") || {};
+
+  if (payload) {
+    const box1 = entities["box1"];
+
+    box1.x = payload.pageX;
+    box1.y = payload.pageY;
+  }
+
+  return entities;
+};
+
+class Renderer extends PureComponent {
+  render() {
+    return (
+      <div
+        style={{ ...this.props.properties, position: "absolute", left: this.props.x, top: this.props.y }}
+      />
+    )
+  }
+}*/
 
 const interaction: InteractionPackage = {
   id: "engine",
