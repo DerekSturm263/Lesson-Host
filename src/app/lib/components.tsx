0db14725eb31016d43d5667e65d1ca55b97a2f10
Derek <dereksturm263@gmail.com>
2025-11-16 06:21:20 +0000

    e

--- src/app/lib/components.tsx
+++ src/app/lib/components.tsx
@@ -1185,6 +1185,7 @@ function TypeSwitcher({ elementID, type, setType }: { elementID: ElementID, type
             key={index}
             value={item.id}
           >
+            <ListItemButton>
               <ListItemIcon>
                 <item.icon />
               </ListItemIcon>
@@ -1192,6 +1193,7 @@ function TypeSwitcher({ elementID, type, setType }: { elementID: ElementID, type
               <ListItemText>
                 {item.prettyName}
               </ListItemText>
+            </ListItemButton>
           </MenuItem>
         )))}
       </Select>
